07cf1db5162c7e909749f5551374b5e6ad38cb4f
the-detention-dodger <tomturns67@gmail.com>
2026-09-03 00:16:07 +0000

    Birthday homepage

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Tom 67';
-export const SITE_DESCRIPTION = 'Welcome to chill space!';
